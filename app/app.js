bb0435a80f2e7ad0d9f8c329030b8b1a733528df
root <root@DESKTOP-3GPMMQF.localdomain>
2026-02-21 13:31:21 +0530

    modified application

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Kubernetes CI/CD! Created by Aravind');
+  res.send('Hello from CI/CD! Created by Aravind');
 });
 
 app.get('/healthz', (req, res) => res.status(200).send('ok'));
